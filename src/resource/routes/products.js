f8591d51d3c45edd360f7bf7a9f4c6085304b06c
@nhTuan <tuan30vh@gmail.com>
2023-01-10 13:23:57 +0700

    update api product

--- src/resource/routes/products.js
+++ src/resource/routes/products.js
@@ -1,8 +1,11 @@
-const accountController = require('../controllers/products');
 const express = require('express');
+const productControler = require('../controllers/products');
 
 const productRoutes = express.Router();
 
-productRoutes.post("/create-product", accountController.createProduct);
-
+productRoutes.post("/create-product", productControler.createProduct);
+productRoutes.delete("/delete-product/:id",productControler.deleteProduct);
+productRoutes.post("/edit-product/:productId",productControler.editProduct);
+productRoutes.get("/get-product-detail/:productId",productControler.getProductDetail);
+productRoutes.get("/search/:key", productControler.search)
 module.exports =  productRoutes;
