7f13cbe7aadfc4c26aac56bd6c5c97675a996881
@nhTuan <tuan30vh@gmail.com>
2023-02-01 01:02:04 +0700

    update get all users

--- src/resource/controllers/account.js
+++ src/resource/controllers/account.js
@@ -186,4 +186,15 @@ accountController.editInfo = async(req, res, next) =>{
         });
     }
 }
+accountController.getAllUser= async(req, res, next) => {
+    try{
+        let users = await accountModel.find()
+        return res.status(httpStatus.OK).json({
+            data: users,
+        });
+    }catch(error){
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message: error.message});
+    }
+
+}
 module.exports = accountController;
--- src/resource/routes/account.js
+++ src/resource/routes/account.js
@@ -7,5 +7,6 @@ accountRoutes.post("/signup", accountController.signup);
 accountRoutes.post("/login", accountController.login);
 accountRoutes.post("/change-password", accountController.changePassword);
 accountRoutes.post("/get-user", accountController.getUserById);
-accountRoutes.post("/edit-user",accountController.editInfo)
+accountRoutes.post("/edit-user",accountController.editInfo);
+accountRoutes.post("/get-all-users",accountController.getAllUser);
 module.exports =  accountRoutes;
